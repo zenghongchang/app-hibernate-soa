7f274a8a55da4d8230af5ac8f281c0307483b05a
zenghongchang <zhc_hnust@outlook.com>
2017-11-14 09:37:45 +0800

    2017-11-14 09:37:42

---
+++ app-hibernate-soa/src/main/java/edu/hnust/application/controller/inteceptor/AuthenticationInteceptor.java
@@ -0,0 +1,42 @@
+package edu.hnust.application.controller.inteceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * SOA拦截器
+ * 
+ * @author Henry(fba02)
+ * @version [版本号, 2017年11月14日]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class AuthenticationInteceptor implements HandlerInterceptor {
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+        throws Exception {
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)) {
+            return false;
+        }
+        System.out.println(token);
+        return true;
+    }
+    
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+        throws Exception {
+        
+    }
+    
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+        throws Exception {
+        
+    }
+}
